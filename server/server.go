cb733a0d9b0285195e8aac7ed674329e62bae416
Wanderson <wandersonduarte.br@gmail.com>
2022-12-30 14:39:41 -0300

    Implementação do server. Savar no banco em andamento

--- server/server.go
+++ server/server.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
 
 /* TODO:
  O server.go deverá consumir a API contendo o câmbio de Dólar e Real no endereço: https://economia.awesomeapi.com.br/json/last/USD-BRL e em seguida deverá retornar no formato JSON o resultado para o cliente.
@@ -11,6 +18,102 @@ O endpoint necessário gerado pelo server.go para este desafio será: /cotacao e
 
 */
 
+type Cotacao struct {
+	Usdbrl struct {
+		Code       string `json:"code"`
+		Codein     string `json:"codein"`
+		Name       string `json:"name"`
+		High       string `json:"high"`
+		Low        string `json:"low"`
+		VarBid     string `json:"varBid"`
+		PctChange  string `json:"pctChange"`
+		Bid        string `json:"bid"`
+		Ask        string `json:"ask"`
+		Timestamp  string `json:"timestamp"`
+		CreateDate string `json:"create_date"`
+	} `json:"USDBRL"`
+}
+
 func main() {
-	fmt.Println("server on")
+	http.HandleFunc("/cotacao", handlerCotacao)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handlerCotacao(w http.ResponseWriter, r *http.Request) {
+
+	route := r.URL.Path
+	if route != "/cotacao" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	cot, err := GetCotacao()
+	if err != nil {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+
+	err = salvar(cot)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	cotJson, err := json.Marshal(cot)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(cotJson)
+
+}
+
+func salvar(cot *Cotacao) error {
+	dsn := "root:root@tcp(localhost:3306)/goexpert"
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*10)
+	defer cancel()
+
+	stmt, err := db.Prepare("insert into cota")
+
+	return nil
+}
+
+func GetCotacao() (*Cotacao, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*200)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx,
+		"GET",
+		"https://economia.awesomeapi.com.br/json/last/USD-BRL",
+		nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer req.Body.Close()
+
+	resp, err := ioutil.ReadAll(req.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var cot Cotacao
+	err = json.Unmarshal(resp, &cot)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cot, nil
+
 }
